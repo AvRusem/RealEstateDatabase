efd6d73ef7120c6996367d9fcbb641506f9a4361
Andrey Bakov <andrey.bakov.2006@gmail.com>
2025-02-27 17:45:04 +0300

    feature: add models

--- app/src/main/java/org/example/model/FlatRoom.java
+++ app/src/main/java/org/example/model/FlatRoom.java
@@ -1,6 +1,33 @@
 package org.example.model;
 
 public class FlatRoom extends Entity {
-    // конструктор с super(address, area),
-    // оверрайдом абстрактных методов и гет/сет уникальных характеристик
+    private int floor;
+    private int flatNumber;
+
+    public FlatRoom(String address, double area, int floor, int flatNumber) {
+        super(address, area);
+        this.floor = floor;
+        this.flatNumber = flatNumber;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(int flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Floor: " + floor + ", Flat number: " + flatNumber;
+    }
 }
