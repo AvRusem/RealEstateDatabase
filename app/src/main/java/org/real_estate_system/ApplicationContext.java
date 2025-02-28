bed0dbed39908335ea319c9023ec83cfae4d714d
AvRusem <avzalor@gmail.com>
2025-02-28 19:44:36 +0300

    small changes

--- app/src/main/java/org/real_estate_system/ApplicationContext.java
+++ app/src/main/java/org/real_estate_system/ApplicationContext.java
@@ -1,19 +1,35 @@
 package org.real_estate_system;
 
+import org.real_estate_system.io.AbstractDialog;
+import org.real_estate_system.io.FlatRoomDialog;
+import org.real_estate_system.io.HouseDialog;
+import org.real_estate_system.io.OfficeDialog;
 import org.real_estate_system.model.FlatRoom;
 import org.real_estate_system.model.House;
 import org.real_estate_system.model.Office;
 import org.real_estate_system.repository.Repository;
+import org.real_estate_system.repository.file.FlatRoomRepository;
+import org.real_estate_system.repository.file.HouseRepository;
+import org.real_estate_system.repository.file.OfficeRepository;
 
 public class ApplicationContext {
     private Repository<FlatRoom> flatRoomRepository;
     private Repository<House> houseRepository;
     private Repository<Office> officeRepository;
 
+    private AbstractDialog<FlatRoom> flatRoomDialog;
+    private AbstractDialog<House> houseDialog;
+    private AbstractDialog<Office> officeDialog;
+
     public ApplicationContext() {
-        flatRoomRepository = new Repository<>();
-        houseRepository = new Repository<>();
-        officeRepository = new Repository<>();
+        String repoName = getRepo();
+        flatRoomRepository = new FlatRoomRepository(repoName + "/flat_room");
+        houseRepository = new HouseRepository(repoName + "/house");
+        officeRepository = new OfficeRepository(repoName + "office");
+
+        flatRoomDialog = new FlatRoomDialog(flatRoomRepository);
+        houseDialog = new HouseDialog(houseRepository);
+        officeDialog = new OfficeDialog(officeRepository);
     }
 
     public Repository<FlatRoom> getFlatRoomRepository() {
@@ -28,4 +44,28 @@ public class ApplicationContext {
         return officeRepository;
     }
 
+    public AbstractDialog<FlatRoom> getFlatRoomDialog() {
+        return flatRoomDialog;
+    }
+
+    public AbstractDialog<House> getHouseDialog() {
+        return houseDialog;
+    }
+    public AbstractDialog<Office> getOfficeDialog() {
+        return officeDialog;
+    }
+
+    private String getRepo() {
+        String repo = System.getProperty("Repository");
+        if (repo == null)
+            throw new MissingEnvironmentVariable("Can not fine Repository variable in .env");
+        
+        return repo;
+    }
+}
+
+class MissingEnvironmentVariable extends RuntimeException {
+    public MissingEnvironmentVariable(String error) {
+        super(error);
+    }
 }
